f2f209d0d986bf57b614d6186311286e29779265
Nurat Lawal <nurahawwal@yahoo.com>
2024-05-07 19:48:42 +0100

    added more items to homepage

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,25 +3,49 @@ const anthemButton = document.getElementById("anthem-btn-show")
 const anthem = document.getElementById("anthem-container")
 const hideAnthem = document.getElementById("anthem-btn-hide")
 const playAnthemButton = document.getElementById("anthem-btn-play")
-const anthemAudio = document.getElementById("audio-container")
+const anthemAudioContainer = document.getElementById("audio-container")
+const anthemAudio = document.getElementById("anthem-play")
 
 
-window.addEventListener("click", (e) => {
+// window.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const targetElement = e.target;
+//    if (targetElement == anthemButton){
+//         if (anthem.classList.contains("displayed")) {
+//             anthem.classList.remove("displayed")
+//         }
+//         else {
+//             anthem.classList.add("displayed")
+//         }
+//    }   
+//    if (targetElement == hideAnthem){
+//       anthem.classList.remove("displayed")       
+//    }     
+//    if (targetElement == playAnthemButton){
+//       anthemAudio.classList.add("displayed")       
+//       playAnthemButton.style.display = "none";    
+//    }     
+// })
+
+anthemButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const targetElement = e.target;
-   if (targetElement == anthemButton){
         if (anthem.classList.contains("displayed")) {
             anthem.classList.remove("displayed")
         }
         else {
             anthem.classList.add("displayed")
         }
-   }   
-   if (targetElement == hideAnthem){
-      anthem.classList.remove("displayed")       
-   }     
-   if (targetElement == playAnthemButton){
-      anthemAudio.classList.add("displayed")       
-      playAnthemButton.style.display = "none";    
-   }     
+})
+
+hideAnthem.addEventListener("click", (e) =>{
+    e.preventDefault();
+     anthem.classList.remove("displayed")         
+})
+
+playAnthemButton.addEventListener("click", (e) =>{
+    e.preventDefault();
+     anthemAudioContainer.classList.add("displayed") 
+    anthemAudio.setAttribute("autoplay", true)
+    anthemAudio.load();
+    playAnthemButton.style.display = "none";              
 })
