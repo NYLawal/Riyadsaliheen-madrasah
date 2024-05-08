55bec0d535206bddf0e4114fb08e442a77f4c059
Nurat Lawal <nurahawwal@yahoo.com>
2024-05-08 16:28:26 +0100

    added programmes page

--- frontend/js/about.js
+++ frontend/js/about.js
@@ -2,6 +2,6 @@
 aboutusButton = document.getElementById("aboutus-button");
 aboutusButton.addEventListener("click", (e) =>{
     e.preventDefault();
-    window.location.href = "http://127.0.0.1:5500/RiyadNew/frontend/programmes.html"      
-    // window.location.href = "https://madrasatu-riyadsaliheen.netlify.app/index.html"      
+    // window.location.href = "http://127.0.0.1:5500/RiyadNew/frontend/programmes.html"      
+    window.location.href = "https://madrasatu-riyadsaliheen.netlify.app/programmes.html"      
 })
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -53,6 +53,6 @@ playAnthemButton.addEventListener("click", (e) =>{
 programmeButton = document.getElementById("hero-button");
 programmeButton.addEventListener("click", (e) =>{
     e.preventDefault();
-    window.location.href = "http://127.0.0.1:5500/RiyadNew/frontend/programmes.html"      
-    // window.location.href = "https://madrasatu-riyadsaliheen.netlify.app/index.html"      
+    // window.location.href = "http://127.0.0.1:5500/RiyadNew/frontend/programmes.html"      
+    window.location.href = "https://madrasatu-riyadsaliheen.netlify.app/programmes.html"      
 })
