44115f3d2434e459d27be22c9b068cf5fc4c1ff5
Nurat Lawal <nurahawwal@yahoo.com>
2024-05-09 08:36:39 +0100

    added support and calendar pages

--- frontend/js/about.js
+++ frontend/js/about.js
@@ -5,3 +5,9 @@ aboutusButton.addEventListener("click", (e) =>{
     // window.location.href = "http://127.0.0.1:5500/RiyadNew/frontend/programmes.html"      
     window.location.href = "https://madrasatu-riyadsaliheen.netlify.app/frontend/programmes.html"      
 })
+donateButton = document.getElementById("support-button");
+donateButton.addEventListener("click", (e) =>{
+    e.preventDefault();
+    // window.location.href = "http://127.0.0.1:5500/RiyadNew/frontend/programmes.html"      
+    window.location.href = "https://madrasatu-riyadsaliheen.netlify.app/frontend/donate.html"      
+})
