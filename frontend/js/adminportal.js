6a1915f0d767abbce09a991c371a708feab8cee5
Nurat Lawal <nurahawwal@yahoo.com>
2024-05-16 04:47:32 +0100

    updated admin portal

--- frontend/js/adminportal.js
+++ frontend/js/adminportal.js
@@ -1,31 +1,67 @@
 const baseUrl = "https://result-proc-system.onrender.com/api/v1"
 // const baseUrl = "http://localhost:5000/api/v1"
 
+const sidebar = document.getElementById("bsbSidebar1")
+const toggler= document.getElementById("toggler-icon")
+
 const addStafferLink= document.getElementById("add-staffer")
 const addStafferForm = document.getElementById("staffadd-form")
 const addStudentLink= document.getElementById("add-student")
 const addStudentForm = document.getElementById("studentadd-form")
 
-const submitButton = document.getElementById("submit-btn")
-const emailInput = document.getElementById("email")
-const nameInput = document.getElementById("staffName")
+const staffTitleInput = document.getElementById("staff-title")
+const staffNameInput = document.getElementById("staff-name")
+const staffEmailInput = document.getElementById("staff-email")
+const staffGenderInput = document.getElementById("staff-gender")
+const staffStreetlInput = document.getElementById("staff-street")
+const staffCityInput = document.getElementById("staff-city")
+const staffStateInput = document.getElementById("staff-state")
+const staffPhoneInput = document.getElementById("staff-phone")
+const classLabel = document.getElementById("class-label")
+const teacherClassInput = document.getElementById("teacher-class")
 const staffRole = document.getElementById("role-drpdwn")
-const logoutLink= document.getElementById("logout")
+
 const cancelLink= document.getElementById("cancel-btn")
 const closeLink= document.getElementById("close-btn")
 const clearStdFrmLink= document.getElementById("clearfrm-btn")
 const closeStdFrmLink= document.getElementById("closefrm-btn")
 
+const submitButton = document.getElementById("submit-btn")
+const sendButton = document.getElementById("send-btn")
+
+const logoutLink= document.getElementById("logout")
+
+
+
+toggler.addEventListener("click", (e) => {
+    sidebar.style.display = "block"
+});
+
 // display add staffer form
 addStafferLink.addEventListener("click", (e) => {
     e.preventDefault(); 
     addStafferForm.style.display = "block"
+    sidebar.style.display = "none"
 });
 
 // display add student form
 addStudentLink.addEventListener("click", (e) => {
     e.preventDefault(); 
     addStudentForm.style.display = "block"
+    sidebar.style.display = "none"
+});
+
+// display class input if role is teacher
+staffRole.addEventListener("change", (e) => {
+    e.preventDefault(); 
+    if (staffRole.value == "teacher"){
+        teacherClassInput.style.display = "block"
+        classLabel.style.display = "block"
+    }
+    else {
+        teacherClassInput.style.display = "none"
+        classLabel.style.display = "none"
+    } 
 });
 
 // add a staff member - admin, bursar or teacher
@@ -73,7 +109,31 @@ const addStaff = (staffInfo) => {
         });
 };
 
+// submit stafff form
 submitButton.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    const stafferName = staffTitleInput.value + "" + staffNameInput.value;
+    const email = staffEmailInput.value;
+    const gender = staffGenderInput.value;
+    const address = staffStreetlInput.value + " " + staffCityInput.value + " " + staffStateInput.value;
+    const phoneNumber = staffPhoneInput.value;
+    const teacherClass = teacherClassInput.value;
+    const role = staffRole.value;
+    
+        const formData = {
+            stafferName,
+            email, 
+            gender,
+            address,
+            phoneNumber,
+            teacherClass,
+            role
+        }
+        addStaff(formData);    
+});
+
+// submit student form
+sendButton.addEventListener("click", (e) => {
     e.preventDefault(); 
     const email = emailInput.value;
     const stafferName = nameInput.value;
